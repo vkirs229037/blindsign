8994a1ee94c6cb7c66ce9bfee72da30ad8ea0c6b
vkirs229037 <vova.kirsanov.03@mail.ru>
2025-04-26 14:33:05 +0300

    исправил ссылку на бд

--- app/config.py
+++ app/config.py
@@ -5,4 +5,4 @@ app_dir = os.path.abspath(os.path.dirname(__file__))
 class BaseConfig:
     # Ключ для генерации CSFR-токена
     SECRET_KEY = os.environ.get("SECRET_KEY") or "12345678qwerty"
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URI") or os.path.dirname(app_dir) + os.path.sep + "data" + os.path.sep + "db.sqlite3"
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URI") or "sqlite:" + os.path.sep * 3 + os.path.dirname(app_dir) + os.path.sep + "data" + os.path.sep + "db.sqlite3"
