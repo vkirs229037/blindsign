4ad0d0325665205f287ded4d2a6522d8f97c03f1
vkirs229037 <vova.kirsanov.03@mail.ru>
2025-04-26 13:35:10 +0300

    Конфиг теперь в отдельном файле

--- app/app.py
+++ app/app.py
@@ -5,7 +5,7 @@ from flask_login import LoginManager, UserMixin, current_user, login_required
 
 app = Flask(__name__)
 app.debug = True
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:////home/vl/Универ/Семестр 8/КМЗИ/Практики/РЗ/data/users.db"
+app.config.from_object("config.BaseConfig")
 db = SQLAlchemy(app)
 login_manager = LoginManager(app)
 login_manager.login_view = "login"
---
+++ app/config.py
@@ -0,0 +1,8 @@
+import os
+
+app_dir = os.path.abspath(os.path.dirname(__file__))
+
+class BaseConfig:
+    # Ключ для генерации CSFR-токена
+    SECRET_KEY = os.environ.get("SECRET_KEY") or "12345678qwerty"
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URI") or os.path.dirname(app_dir) + os.path.sep + "data" + os.path.sep + "db.sqlite3"
